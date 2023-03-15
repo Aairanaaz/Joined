ad35c2983aadfab3f5994d2c94b33ec5ce4bc692
Shevilll <ahmadfaraz00710@gmail.com>
2023-03-15 16:09:40 +0530

    ..

---
+++ soh.py
@@ -0,0 +1,9 @@
+# hastag comment k lye..jo bhi line hashtag se start hoga uska koi kaam nh rhta h wo bs code clear krne k lye hota h
+
+print('Hello') # ye string type
+print(2) # ye int type
+
+# tm variable bhi bna skti h..uske lye bhi rules hota h lekin
+
+text = 'apple' # ye string tmhra text variable m store hogy ab tm isko direct print kr skti h
+print(text)
